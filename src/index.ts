8c278d0ed50ad060388420a5f8f784f195da0bf5
aaronb97 <aaron9becker7@gmail.com>
2022-01-16 14:14:29 -0500

    load rocket on screen

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import './styles.css';
+import rocketImagePath from '../images/rocket.png';
 
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
@@ -10,25 +11,26 @@ const app = new PIXI.Application({ resizeTo: window });
 // can then insert into the DOM.
 document.body.appendChild(app.view);
 
-// load the texture we need
-// app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
-//   // This creates a texture from a 'bunny.png' image.
-//   const bunny = new PIXI.SHAPES.CIRC();
+app.loader.add('rocket', rocketImagePath).load((loader, resources) => {
+  // This creates a texture from a 'rocket.png' image.
+  console.log(resources);
+  const rocket = new PIXI.Sprite(resources.rocket.texture);
 
-//   // Setup the position of the bunny
-//   bunny.x = app.renderer.width / 2;
-//   bunny.y = app.renderer.height / 2;
+  rocket.width = 50;
+  rocket.height = 50;
+  // Setup the position of the rocket
 
-//   // Rotate around the center
-//   bunny.anchor.x = 0.5;
-//   bunny.anchor.y = 0.5;
+  // Rotate around the center
+  rocket.anchor.x = 0.5;
+  rocket.anchor.y = 0.5;
 
-//   // Add the bunny to the scene we are building.
-//   app.stage.addChild(bunny);
+  // Add the rocket to the scene we are building.
+  app.stage.addChild(rocket);
 
-//   // Listen for frame updates
-//   app.ticker.add(() => {
-//     // each frame we spin the bunny around a bit
-//     bunny.rotation += 0.01;
-//   });
-// });
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the rocket around a bit
+    rocket.x = app.renderer.width / 2;
+    rocket.y = app.renderer.height / 2;
+  });
+});
